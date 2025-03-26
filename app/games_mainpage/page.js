2750ca6e266aa65f792fbcad6ee36bc2594859fa
Naushin Rahman <naushin.r@gmail.com>
2025-03-25 23:46:44 -0400

    sequence memory game is working!

--- app/games_mainpage/page.js
+++ app/games_mainpage/page.js
@@ -4,71 +4,77 @@ import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
 	subsets: ["latin"],
-	weight: ["400", "500", "700"],
+	weight: ["400", "500", "600", "700"],
   })
 
 export default function Home() {
-  return (
-    <div className={`${montserrat.className}`}>
-    	<div className="flex flex-row justify-between">
-			<div>
-				<Image src="icon.svg" className="fixed rounded-xl" width={0} height={0} 
-				style={{ width: "10%", height: "auto", paddingLeft: "15px", paddingTop: "20px" }} alt="Refresh Logo"/>
-			</div>
+	return (
+		<div className={`${montserrat.className}`}>
+			<div className="flex flex-row justify-between">
+				<div>
+					<Image src="icon.svg" className="fixed rounded-xl" width={0} height={0} 
+					style={{ width: "10%", height: "auto", paddingLeft: "15px", paddingTop: "20px" }} alt="Refresh Logo"/>
+				</div>
 
-			<div>
-				<a href="/">
-					<button
-						className="font-medium rounded-full mt-5 mr-6 text-center text-white duration-500 hover:scale-125"
-						type="button" 
-						style={{ 
-							backgroundColor: "#D44087",  
-							padding: "5px 27px",
-							fontSize: "1.7rem",
-							position: "fixed", 
-							top: "7px",
-							right: "10px",
-							letterSpacing: '1px', 
-							}} >
-						RETURN
-					</button>
-				</a>
-			</div>
-		</div>
-
-    	<div className="flex justify-center space-x-5">
-      		<Image src="/head.svg" width={0} height={0} priority={true}
-			style={{ position: 'fixed', width: "15%", bottom: "0", left: "0", }} alt="floating head"/>
-
-			<div className="relative w-full flex flex-col items-center">
-			<Image src="brown_text_box.svg" className="absolute z-0" width={0} height={0} priority={true}
-			style={{ width: "32%", height: "auto", top: "6.5rem", }} alt="Brown Text Box"/>
-
-			<div className="w-full flex justify-center">
-				<div className="absolute z-10 w-[30%]" style={{ right: "35%", top: "8.5rem" }}>
-					<div className="flex flex-col items-center text-center font-bold">
-						<h1 className="text-4xl font-bold">Memory Game Instructions</h1>
-						<p style={{ fontSize: '26px', marginTop: "20px" }}>1. Memorize the order of squares as they light up</p>
-						<p style={{ fontSize: '26px', marginTop: "15px" }}>2. Click the squares in the correct sequence</p>
-						<p style={{ fontSize: '26px', marginTop: "15px" }}>3. Complete all levels to win!</p>
-						<a href="/sequence_game">
-							<button className="rounded-full duration-500 hover:scale-110" 
+				<div>
+					<a href="/">
+						<button
+							className="font-medium rounded-full mt-5 mr-6 text-center text-white duration-500 hover:scale-125"
+							type="button" 
 							style={{ 
-								padding: "5px 27px", 
-								fontSize: "30px", 
-								marginTop: "40px",  
-								backgroundColor: '#b9ddff',
-								color: '#443534',
-								fontWeight: 500,
-							}}>
-								Start Game
-							</button>
-						</a>
+								backgroundColor: "#D44087",  
+								padding: "5px 27px",
+								fontSize: "1.7rem",
+								position: "fixed", 
+								top: "7px",
+								right: "10px",
+								letterSpacing: '1px', 
+								}} >
+							RETURN
+						</button>
+					</a>
+				</div>
+			</div>
+
+			<div className="flex justify-center space-x-5">
+				<Image src="/head.svg" width={0} height={0} priority={true}
+				style={{ position: 'fixed', width: "15%", bottom: "0", left: "0", }} alt="floating head"/>
+
+				<div className="relative w-full flex flex-col items-center">
+				<Image src="brown_text_box.svg" className="absolute z-0" width={0} height={0} priority={true}
+				style={{ width: "32%", height: "auto", top: "6.5rem", }} alt="Brown Text Box"/>
+
+				<div className="w-full flex justify-center">
+					<div className="absolute z-10 w-[30%]" style={{ right: "35%", top: "8.5rem" }}>
+						<div className="flex flex-col items-center text-center font-bold">
+							<h1 className="text-4xl font-bold mb-4">Sequence Memory Game</h1>
+							<div className="bg-[#fffcf0] text-[#443435] rounded-lg ml-5 mr-4">
+								<h2 className="text-3xl mt-[4px]">How to Play:</h2>
+								<p className="text-[25px] mt-[11px]">1. Watch the squares light up in a specific order.</p>
+								<p className="text-[25px] mt-[9px]">2. Remember the order in which the squares lit up.</p>
+								<p className="text-[25px] mt-[9px]">3. Click the squares in that same order.</p>
+								<p className="text-[25px] mt-[9px] mb-[3px]">4. Complete all 5 levels to win!</p>
+							</div>
+							<div className="w-full flex justify-end pr-5">
+								<a href="/sequence_game" className="flex flex-row items-end">
+									<button className="rounded-full duration-500 hover:scale-110" 
+									style={{ 
+										padding: "5px 27px", 
+										fontSize: "30px", 
+										marginTop: "30px",  
+										backgroundColor: '#b9ddff',
+										color: '#443534',
+										fontWeight: 500,
+									}}>
+										Next →
+									</button>
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-			</div>
 		</div>
-    </div>
+	</div>
   );
 };
--- component/sequence_memory_game.jsx
+++ component/sequence_memory_game.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 const montserrat = Montserrat({
 	subsets: ["latin"],
 	weight: ["400", "500", "600", "700"],
-  })
+})
 
 const SequenceMemoryGame = () => {
 	const [gameStarted, setGameStarted] = useState(true);
@@ -14,128 +14,87 @@ const SequenceMemoryGame = () => {
 	const [level, setLevel] = useState(1);
 	const [gameOver, setGameOver] = useState(false);
 	const [isUserTurn, setIsUserTurn] = useState(false);
-	const [displayOver, setDisplayOver] = useState(false);
 	const [playerWon, setPlayerWon] = useState(false);
-	const [squares, setSquares] = useState([
-		{ id: 0, color: "#b9ddff" },
-		{ id: 1, color: "#d7629a" },
-		{ id: 2, color: "#fffcf0" },
-		{ id: 3, color: "#71b0f5" },
-	]);
-
+	const [activeSquare, setActiveSquare] = useState(null);
+	const squares = [
+        { id: 0, color: "#b9ddff" },
+        { id: 1, color: "#d7629a" },
+        { id: 2, color: "#fffcf0" },
+        { id: 3, color: "#71b0f5" },
+    ];
 	const winLevel = 6;
-
+	
 	useEffect(() => {
-		if(gameStarted) {
-			increaseSequence();
-		}
-	}, [gameStarted]);
-	
+        if (gameStarted) {
+            startNewLevel();
+        }
+    }, [gameStarted]);
 
-	const increaseSequence = () => {
-		setSequence((prevSeq) => {
-			const lastTwo = prevSeq.slice(-2);
-			let newNum;
-			do {
-				newNum = Math.floor(Math.random() * 4);
-			} while (newNum === lastTwo[0] && newNum === lastTwo[1]);
-	
-			return [...prevSeq, newNum];
-		});
-	};
-
-
-	const highlightSquare = (id) => {
-        setSquares((prevSquares) =>
-            prevSquares.map((sq) =>
-                sq.id === id ? { ...sq, color: "#FFFF16" } : sq
-            )
-        );
-
+    const startNewLevel = () => {
+        setIsUserTurn(false);
         setTimeout(() => {
-            setSquares((prevSquares) =>
-                prevSquares.map((sq) =>
-                    sq.id === id ? { ...sq, color: squares[id].color } : sq
-                )
-            );
+            addToSequence();
         }, 1000);
     };
-	
 
-	const gameLevel = () => {
-		let delay = 0;
-		sequence.forEach((highlightedDiv) => {
-			setTimeout(() => {
-				highlightSquare(highlightedDiv);
-			}, delay);
-			
-			delay+=1000;
-		});
+    const addToSequence = () => {
+        setSequence((prev) => {
+            const newNum = Math.floor(Math.random() * 4);
+            return [...prev, newNum];
+        });
+    };
 
-		let totalRuntime = (sequence.length*1000);
-		setTimeout(() => {
-			setDisplayOver(true);
-		}, totalRuntime);
-	}
-
-
-	useEffect(() => {
-		if (displayOver) {
-			setIsUserTurn(true)
-		}
-	}, [displayOver])
-
-
-	const squareClick = (id) => {
-		if (displayOver === true && isUserTurn) {
-			if (id === sequence[currentIndex]) {
-				setCurrentIndex((prev) => prev + 1);
-
-                if (currentIndex + 1 === sequence.length) {
-                    setTimeout(() => {
-						setDisplayOver(false);
-                        setLevel((prev) => prev + 1);
-                        setCurrentIndex(0);
-                        increaseSequence();
-                    }, 1000);
-				}
-			}
-			else {
-				setDisplayOver(false);
-				setGameOver(true);
-				setGameStarted(false);
-				setIsUserTurn(false);
-				setSequence([]);
-				setCurrentIndex(0);
-				setLevel(1);
-			}
-		}
-	}
-
-	const winAchieved = () => {
-		setDisplayOver(false);
-		setGameOver(true);
-		setGameStarted(false);
-		setIsUserTurn(false);
-		setSequence([]);
-		setCurrentIndex(0);
-		setLevel(1);
-		setPlayerWon(true);
-	}
-
-	function restartGame() {
-		setGameStarted(true);
-		setGameOver(false);
-	}
-
-	useEffect(() => {
-		if (gameStarted && sequence.length > 0 && sequence.length < winLevel) {
-            gameLevel();
-        }
+    useEffect(() => {
 		if (sequence.length === winLevel) {
-			winAchieved();
+			setPlayerWon(true);
+			setGameOver(true);
+			setGameStarted(false);
 		}
+
+        if (sequence.length > 0 && sequence.length < winLevel) {
+            playSequence();
+        }
     }, [sequence]);
+
+    const playSequence = () => {
+        let delay = 0;
+        sequence.forEach((num, i) => {
+            setTimeout(() => {
+                setActiveSquare(num);
+                setTimeout(() => setActiveSquare(null), 500);
+            }, delay);
+            delay += 1000;
+        });
+        setTimeout(() => setIsUserTurn(true), delay);
+    };
+
+    const handleSquareClick = (id) => {
+        if (!isUserTurn || gameOver) return;
+
+        if (id === sequence[currentIndex]) {
+            setCurrentIndex((prev) => prev + 1);
+            if (currentIndex + 1 === sequence.length) {
+				setCurrentIndex(0);
+				startNewLevel();
+				if (!gameOver) {
+					setLevel((prev) => prev + 1);
+				}
+            }
+        } else {
+            setGameOver(true);
+            setGameStarted(false);
+            setSequence([]);
+        }
+    };
+
+    const restartGame = () => {
+        setGameStarted(true);
+        setGameOver(false);
+        setPlayerWon(false);
+        setSequence([]);
+        setCurrentIndex(0);
+        setLevel(1);
+    };
 
 
 	return (
@@ -143,61 +102,80 @@ const SequenceMemoryGame = () => {
 		style={{
 			zIndex: 50, 
 			position: 'relative',
-			marginTop: "0.25rem",
 			}}
 		>
 
-		<p style={{paddingBottom: "15px", fontSize: '36px', fontWeight: 600}}>
-			{playerWon && gameOver ? "You Win!" : gameOver ? "Game Over" : `Level ${level}`}
+		<p style={{paddingBottom: "15px", fontSize: '38px', fontWeight: 600}}>
+			{playerWon ? "You Win!" : gameOver ? "Game Over" : `Level ${level}`}
 		</p>
 
 		<div
 			style={{
 				display: "grid",
 				gridTemplateColumns: "repeat(2, 1fr)",
-				gap: "20px",
+				gap: "24px",
 				width: "100%",
 			}}
 		>
 			{squares.map((square) => (
 				<div
 				key={square.id}
-				onClick={() => squareClick(square.id)}
+				onClick={() => handleSquareClick(square.id)}
 				style={{
-					backgroundColor: square.color,
+					backgroundColor: activeSquare === square.id ? "#FFFF16" : square.color,
 					padding: "40px",
-					height: "10rem",
-					width: "10rem",
+					height: "10.5rem",
+					width: "10.5rem",
 					display: "flex",
 					borderRadius: "20px",
-					cursor: "pointer"
+					cursor: isUserTurn ? "pointer" : "default",
 				}}>
 				</div>
 			))}
 		</div>
 		<div>
-			{ !gameStarted ? (
-				<button 
-				onClick={() => restartGame()}
-				style={{
-					padding: "5px 22px",
-					marginTop: "30px",
-					fontSize: "30px", 
-					backgroundColor: "#9347E8",
-					color: "white", 
-					cursor: "pointer",
-					fontWeight: 600,
-					letterSpacing: "1px",
-					borderRadius: "40px",
-					transition: "transform 0.5s ease-in-out"
-				}}
-				onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
-				onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
-				>
-				Restart Game
-			 	</button>
-			) : (<></>)
-				}
+			{ (gameOver || playerWon) && (
+				<div 
+				style={{ 
+					display: "flex", 
+					justifyContent: "space-between", 
+					fontSize: "32px",
+					marginTop: "32px",
+					fontWeight: 600, 
+					}}>
+					<button 
+					onClick={() => restartGame()}
+					style={{
+						padding: "5px 22px", 
+						backgroundColor: "#A30000",
+						color: "white", 
+						cursor: "pointer",
+						borderRadius: "40px",
+						transition: "transform 0.5s ease-in-out"
+					}}
+					onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
+					onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
+					>
+					Restart
+					</button>
+					<a href="/games_mainpage">
+						<button 
+						style={{
+							padding: "5px 22px",
+							backgroundColor: '#b9ddff',
+							color: '#443534',
+							cursor: "pointer",
+							borderRadius: "40px",
+							transition: "transform 0.5s ease-in-out"
+						}}
+						onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
+						onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
+						>
+						Back ↩
+						</button>
+					</a>
+				</div>
+			)}
 		</div>
 	</div>
 	);
